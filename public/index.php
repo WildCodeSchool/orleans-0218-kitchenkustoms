fd65bbb08b6c0557c32fba5ca533c86331ea189d
Gauthier Arnaud <Vilc86@hotmail.com>
2018-04-05 10:53:39 +0200

    Ajout de la Google Map dans le footer

--- public/index.php
+++ public/index.php
@@ -12,6 +12,6 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../app/db.php';
+//require_once __DIR__ . '/../app/db.php';
 require_once __DIR__ . '/../app/config.php';
 require_once __DIR__ . '/../app/dispatcher.php';
