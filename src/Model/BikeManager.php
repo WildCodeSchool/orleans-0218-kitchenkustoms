bf8f2e70b9c0f6ab5fc5e484f290b4468d79c952
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-04-20 09:16:36 +0200

    suppression des variables temporaires de la fonction updateBike

--- src/Model/BikeManager.php
+++ src/Model/BikeManager.php
@@ -37,19 +37,6 @@ class BikeManager extends AbstractManager
 
     public function updateBike(Bike $bike)
     {
-        $id = $bike->getId();
-        $name = $bike->getName();
-        $description = $bike->getDescription();
-        $photo_before = $bike->getPhotoBefore();
-        $photo_after = $bike->getPhotoAfter();
-        $rate_look = $bike->getRateLook();
-        $rate_practice = $bike->getRatePractice();
-        $rate_fun = $bike->getRateFun();
-        $price = $bike->getPrice();
-        $sold = $bike->getSold();
-        $is_kustom = $bike->getIsKustom();
-        $is_sold = $bike->getIsSold();
-
         $query = 'UPDATE ' . $this->table .
                 ' SET name=:name, description=:description, photo_before=:photo_before, photo_after=:photo_after, 
                 rate_look=:rate_look, rate_practice=:rate_practice, rate_fun=:rate_fun, price=:price, sold=:sold, 
@@ -58,18 +45,18 @@ class BikeManager extends AbstractManager
 
         $prepare = $this->pdoConnection->prepare($query);
 
-        $prepare->bindValue('id', $id, \PDO::PARAM_INT);
-        $prepare->bindValue('name', $name, \PDO::PARAM_STR);
-        $prepare->bindValue('description', $description);
-        $prepare->bindValue('photo_before', $photo_before);
-        $prepare->bindValue('photo_after', $photo_after);
-        $prepare->bindValue('rate_look', $rate_look);
-        $prepare->bindValue('rate_practice', $rate_practice);
-        $prepare->bindValue('rate_fun', $rate_fun);
-        $prepare->bindValue('price', $price);
-        $prepare->bindValue('sold', $sold, \PDO::PARAM_BOOL);
-        $prepare->bindValue('is_kustom', $is_kustom, \PDO::PARAM_BOOL);
-        $prepare->bindValue('is_sold', $is_sold, \PDO::PARAM_BOOL);
+        $prepare->bindValue('id', $bike->getId(), \PDO::PARAM_INT);
+        $prepare->bindValue('name', $bike->getName(), \PDO::PARAM_STR);
+        $prepare->bindValue('description', $bike->getDescription());
+        $prepare->bindValue('photo_before', $bike->getPhotoBefore());
+        $prepare->bindValue('photo_after', $bike->getPhotoAfter());
+        $prepare->bindValue('rate_look', $bike->getRateLook());
+        $prepare->bindValue('rate_practice', $bike->getRatePractice());
+        $prepare->bindValue('rate_fun', $bike->getRateFun());
+        $prepare->bindValue('price', $bike->getPrice());
+        $prepare->bindValue('sold', $bike->getSold(), \PDO::PARAM_BOOL);
+        $prepare->bindValue('is_kustom', $bike->getIsKustom(), \PDO::PARAM_BOOL);
+        $prepare->bindValue('is_sold', $bike->getIsSold(), \PDO::PARAM_BOOL);
 
         $prepare->execute();
     }
