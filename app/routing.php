73f27b5e08c7f4474e9b74fc0d44a7301fea0063
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-04-26 12:01:39 +0200

    ajout du bouton supprimer sur les categories atelier

--- app/routing.php
+++ app/routing.php
@@ -39,7 +39,7 @@ $routes = [
     'WorkshopCategory' => [
         ['adminIndex', '/admin/atelier/categories', 'GET'],
         ['adminAdd', '/admin/atelier/categories', 'POST'],
-        ['adminDelete', '/admin/atelier/categories/{id:\d+}', 'POST'],
+        ['adminDelete', '/admin/atelier/categories/delete/{id:\d+}', 'POST'],
     ],
     'Pdf'=>[
         ['adminUpdatePdf', '/admin/carte', ['GET', 'POST']],
