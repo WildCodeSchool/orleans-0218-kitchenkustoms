a4799f4b055edecf489d8d36db6ba37af06b5057
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-05-01 12:39:26 +0200

    unions des fichiers de suppression js en un seul

--- public/admin/assets/js/cateringDelete.js
+++
@@ -1,8 +0,0 @@
-$('.deleteButton').click(function (e) {
-    e.preventDefault();
-    let deleteCatering = confirm("Voulez-vous supprimer cet élément ?");
-
-    if (deleteCatering) {
-        $('.delete').submit();
-    }
-});
